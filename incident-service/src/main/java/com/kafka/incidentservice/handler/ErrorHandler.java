80cd2ac60780644020b431c8f95f458f556787d6
Fabio-Alfredo <00210121@uca.edu.sv>
2025-08-08 10:32:36 -0600

    :sparkles: feat: add new handler for errors and new map errors

--- incident-service/src/main/java/com/kafka/incidentservice/handler/ErrorHandler.java
+++ incident-service/src/main/java/com/kafka/incidentservice/handler/ErrorHandler.java
@@ -1,2 +1,61 @@
-package com.kafka.incidentservice.handler;public class ErrorHandler {
+package com.kafka.incidentservice.handler;
+
+import com.kafka.incidentservice.domain.dtos.common.GeneralResponse;
+import com.kafka.incidentservice.uitls.error.ErrorTools;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
+
+@ControllerAdvice
+public class ErrorHandler {
+
+    private final ErrorTools errorTools;
+
+    public ErrorHandler(ErrorTools errorTools) {
+        this.errorTools = errorTools;
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<GeneralResponse> GeneralHandler(Exception ex) {
+        return GeneralResponse.getResponse(HttpStatus.CONFLICT, ex.getMessage());
+    }
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<GeneralResponse> ResourceNotFoundHandler(NoResourceFoundException ex) {
+        return GeneralResponse.getResponse(HttpStatus.NOT_FOUND, ex.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<GeneralResponse> BadRequestHandler(MethodArgumentNotValidException ex) {
+        return GeneralResponse.getResponse(HttpStatus.BAD_REQUEST, errorTools.errorMap(ex.getBindingResult().getFieldErrors()));
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<GeneralResponse> IllegalArgumentHandler(IllegalArgumentException ex) {
+        return GeneralResponse.getResponse(HttpStatus.BAD_REQUEST, ex.getMessage());
+    }
+
+    @ExceptionHandler(IllegalStateException.class)
+    public ResponseEntity<GeneralResponse> IllegalStateHandler(IllegalStateException ex) {
+        return GeneralResponse.getResponse(HttpStatus.BAD_REQUEST, ex.getMessage());
+    }
+
+    @ExceptionHandler(HttpClientErrorException.Forbidden.class)
+    public ResponseEntity<GeneralResponse> forbiddenException(HttpClientErrorException.Forbidden ex) {
+        return GeneralResponse.getResponse(HttpStatus.FORBIDDEN, ex.getMessage());
+    }
+
+    @ExceptionHandler(HttpClientErrorException.Unauthorized.class)
+    public ResponseEntity<GeneralResponse> unauthorizedException(HttpClientErrorException.Unauthorized ex) {
+        return GeneralResponse.getResponse(HttpStatus.UNAUTHORIZED, ex.getMessage());
+    }
+
+    @ExceptionHandler(InternalError.class)
+    public ResponseEntity<GeneralResponse> internalErrorHandler(InternalError ex) {
+        return GeneralResponse.getResponse(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
+    }
 }
--- incident-service/src/main/java/com/kafka/incidentservice/uitls/error/ErrorTools.java
+++ incident-service/src/main/java/com/kafka/incidentservice/uitls/error/ErrorTools.java
@@ -1,2 +1,25 @@
-package com.kafka.incidentservice.uitls.error;public class ErrorTools {
+package com.kafka.incidentservice.uitls.error;
+
+import org.springframework.stereotype.Component;
+import org.springframework.validation.FieldError;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class ErrorTools {
+
+    public Map<String, List<String>>errorMap(List<FieldError>errors){
+        Map<String, List<String>> mapError = new HashMap<>();
+
+        errors.forEach(e -> {
+                    List<String> _errors = mapError.getOrDefault(e.getField(), new ArrayList<>());
+                    _errors.add(e.getDefaultMessage());
+                    mapError.put(e.getField(), _errors);
+
+        });
+        return mapError;
+    }
 }
