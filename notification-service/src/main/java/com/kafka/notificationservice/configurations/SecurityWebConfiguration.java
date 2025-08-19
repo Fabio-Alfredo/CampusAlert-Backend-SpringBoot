8c69b2ac76f87ed9b86b8909798b7db2de9d3cdf
Fabio-Alfredo <00210121@uca.edu.sv>
2025-08-18 22:40:18 -0600

    :sparkles: feat: add configuration by web security

---
+++ notification-service/src/main/java/com/kafka/notificationservice/configurations/SecurityWebConfiguration.java
@@ -0,0 +1,45 @@
+package com.kafka.notificationservice.configurations;
+
+import com.kafka.notificationservice.utils.security.AuthFilterTools;
+import jakarta.servlet.FilterChain;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+public class SecurityWebConfiguration {
+
+    private final AuthFilterTools authFilterTools;
+
+    public SecurityWebConfiguration(AuthFilterTools authFilterTools) {
+        this.authFilterTools = authFilterTools;
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http)throws Exception{
+        http.csrf(csrf-> csrf.disable());
+
+        http.authorizeHttpRequests(auth->
+                auth.requestMatchers("/api/notifications/**").permitAll()
+                        .anyRequest().authenticated()
+        );
+
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        http.exceptionHandling(exception ->
+                exception.authenticationEntryPoint((req, res, ex)->{
+                    res.sendError(
+                            401,
+                            "Unauthorized: " + ex.getMessage()
+                    );
+                })
+        );
+
+        http.addFilterBefore(authFilterTools, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+}
